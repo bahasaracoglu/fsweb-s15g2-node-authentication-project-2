5935234af2e380e681bcb78aa5d2445b685b6fb6
bahasaracoglu <bahasaracoglu@gmail.com>
2023-05-31 21:44:53 +0300

    usernameVarmi

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,4 +1,5 @@
 const { JWT_SECRET } = require("../secrets"); // bu secreti kullanın!
+const usersModel = require("../users/users-model");
 
 const sinirli = (req, res, next) => {
   /*
@@ -32,7 +33,7 @@ const sadece = (role_name) => (req, res, next) => {
   */
 };
 
-const usernameVarmi = (req, res, next) => {
+const usernameVarmi = async (req, res, next) => {
   /*
     req.body de verilen username veritabanında yoksa
     status: 401
@@ -40,6 +41,19 @@ const usernameVarmi = (req, res, next) => {
       "message": "Geçersiz kriter"
     }
   */
+
+  try {
+    let { username } = req.body;
+    const isExist = await usersModel.goreBul({ username: username });
+    if (!isExist[0]) {
+      res.status(401).json({ message: "Geçersiz kriter" });
+    } else {
+      req.currentUser = isExist[0];
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
 };
 
 const rolAdiGecerlimi = (req, res, next) => {
